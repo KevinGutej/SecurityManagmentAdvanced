62d7ac8ca3584a45249d2dd6fa1f00376ef163bf
KevinGutej <76398856+KevinGutej@users.noreply.github.com>
2024-04-26 23:37:12 +0200

    Added another letter and function get string

---
+++ main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+
+using namespace std;
+
+string getString(string generatedLetter1, string generatedLetter2) {
+    string output;
+    int i;
+    int index1 = 0;
+    int index2 = 0;
+    string buffer;
+    for(i =0; i < 6; i++) {
+        while(generatedLetter1[index1] != '\n') {
+            output = output + generatedLetter1[index1];
+            index1++;
+        }
+        output = output + "   ";
+        index1++;
+
+        while(generatedLetter2[index2] != '\n') {
+            output = output + generatedLetter2[index2];
+            index2++;
+        }
+        output = output + "   ";
+        index2++;
+    }
+    return output;
+}
+
+
+string generateLetter(char symbol) {
+    switch(symbol) {
+
+    case 'A':
+        return
+            "     /\\\n"
+            "    /  \\\n"
+            "   /    \\\n"
+            "  /------\\\n"
+            " /        \\\n"
+            "/          \\\n";
+    case 'B':
+        return
+            "|^^^\\\n"
+            "|    |\n"
+            "|___/\n"
+            "|   \\\n"
+            "|    |\n"
+            "|___/\n";
+    }
+}
+
+int main()
+{
+    cout << generateLetter('A') << endl;
+    cout << generateLetter('B') << endl;
+
+    return 0;
+}
